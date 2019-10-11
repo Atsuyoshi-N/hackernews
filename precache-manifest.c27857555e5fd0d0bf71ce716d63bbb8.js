46fec4eceb0044f8e699a13feea34ceee2269299
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-10-12 05:51:21 +0900

    Updates

---
+++ precache-manifest.c27857555e5fd0d0bf71ce716d63bbb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e01afb3c427492baedaf3be646a3677",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "0b71b3e7a0111b742c73",
+    "url": "/hackernews/static/css/main.d0f46bca.chunk.css"
+  },
+  {
+    "revision": "8bb56ec0590907f45d98",
+    "url": "/hackernews/static/js/2.268affa4.chunk.js"
+  },
+  {
+    "revision": "0b71b3e7a0111b742c73",
+    "url": "/hackernews/static/js/main.ebedda2d.chunk.js"
+  },
+  {
+    "revision": "6949de6e00b9294841ce",
+    "url": "/hackernews/static/js/runtime-main.9ae721ae.js"
+  }
+]);
