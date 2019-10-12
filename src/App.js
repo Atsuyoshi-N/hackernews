c6d17ef5901c61312e25a89bb11a01854e1859a5
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-10-13 07:03:17 +0900

    Deal with caching, and handle error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import Table from './Table';
+import Button from './Button';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PATH_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,55 +19,102 @@ class App extends React.Component {
 
     this.state = {
       result: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
   }
 
   setSearchTopStories(result) {
-    this.setState({result});
+    const {hits, page} = result;
+    const {searchKey, results} = this.state;
+
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({
+      results: {...results, [searchKey]: {hits: updatedHits, page}},
+    });
   }
 
   componentDidMount() {
     const {searchTerm} = this.state;
-
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+    this.setState({searchKey: searchTerm});
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss(id) {
+    const {searchKey, results} = this.state;
+    const {hits, page} = results[searchKey];
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
     this.setState({
-      result: {...this.state.result, hits: updatedHits},
+      results: {...results, [searchKey]: {hits: updatedHits, page}},
     });
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    axios(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PATH_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`,
+    )
+      .then(result => this.setSearchTopStories(result.data))
+      .catch(error => this.setState({error}));
   }
 
   onSearchChange(event) {
     this.setState({searchTerm: event.target.value});
   }
 
+  onSearchSubmit(event) {
+    const {searchTerm} = this.state;
+    this.setState({searchKey: searchTerm});
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    event.preventDefault();
+  }
+
   render() {
-    const {searchTerm, result} = this.state;
+    const {searchTerm, results, searchKey, error} = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
 
     return (
       <div className="page">
         <div className="interactions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}>
             Search
           </Search>
         </div>
-        {result && (
-          <Table
-            list={result.hits}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
         )}
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
@@ -70,9 +122,9 @@ class App extends React.Component {
 
 export default App;
 
-const Search = ({value, onChange, children}) => (
-  <form>
-    {children}
+const Search = ({value, onChange, onSubmit, children}) => (
+  <form onSubmit={onSubmit}>
     <input type="text" value={value} onChange={onChange} />
+    <button type="submit">{children}</button>
   </form>
 );
--- src/Table.js
+++ src/Table.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import Button from './Button';
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const Table = ({list, onDismiss}) => {
+  return (
+    <div className="table">
+      {list.map(item => {
+        return (
+          <div key={item.objectID} className="table-row">
+            <span style={{width: '40%'}}>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={{width: '30%'}}>{item.author}</span>
+            <span style={{width: '10%'}}>{item.num_comments}</span>
+            <span style={{width: '10%'}}>{item.points}</span>
+            <span style={{width: '10%'}}>
+              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default class Table extends React.Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div className="table">
-        {list.filter(isSearched(pattern)).map(item => {
-          return (
-            <div key={item.objectID} className="table-row">
-              <span style={{width: '40%'}}>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span style={{width: '30%'}}>{item.author}</span>
-              <span style={{width: '10%'}}>{item.num_comments}</span>
-              <span style={{width: '10%'}}>{item.points}</span>
-              <span style={{width: '10%'}}>
-                <Button onClick={() => onDismiss(item.objectID)}>
-                  Dismiss
-                </Button>
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+export default Table;
